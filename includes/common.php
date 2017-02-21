6d676ee14ff95a54e36f771093a2c48acc456b47
Mike Pretzlaw <m.pretzlaw@inpsyde.com>
2017-02-21 15:40:34 +0100

    QA: VIP coding standards

--- includes/common.php
+++ includes/common.php
@@ -6,20 +6,21 @@
 function tm4mlp_get_current_post_type() {
 	global $post, $typenow, $current_screen;
 
-	//we have a post so we can just get the post type from that
 	if ( $post && $post->post_type ) {
 		return $post->post_type;
-	} //check the global $typenow - set in admin.php
-	elseif ( $typenow ) {
-		return $typenow;
-	} //check the global $current_screen object - set in sceen.php
-	elseif ( $current_screen && $current_screen->post_type ) {
-		return $current_screen->post_type;
-	} //lastly check the post_type querystring
-	elseif ( isset( $_REQUEST['post_type'] ) ) {
-		return sanitize_key( $_REQUEST['post_type'] );
 	}
 
-	//we do not know the post type!
+	if ( $typenow ) {
+		return $typenow;
+	}
+
+	if ( $current_screen && $current_screen->post_type ) {
+		return $current_screen->post_type;
+	}
+
+	if ( isset( $_REQUEST['post_type'] ) ) { // Input var okay
+		return sanitize_key( $_REQUEST['post_type'] ); // Input var okay
+	}
+
 	return null;
 }
