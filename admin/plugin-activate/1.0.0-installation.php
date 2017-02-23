510cc2bd5cfa38e3c77ba0ee3b018279d7021b66
Mike Pretzlaw <m.pretzlaw@inpsyde.com>
2017-02-23 16:34:14 +0100

    Setup: Network wide options

--- admin/plugin-activate/1.0.0-installation.php
+++ admin/plugin-activate/1.0.0-installation.php
@@ -1,5 +1,5 @@
 <?php
 
-if ( ! get_option( 'tm4mlp_api_url' ) ) {
-	update_option( 'tm4mlp_api_url', 'http://46.4.76.165/api' );
+if ( ! get_site_option( 'tm4mlp_api_url' ) ) {
+	update_site_option( 'tm4mlp_api_url', 'http://46.4.76.165/api' );
 }
