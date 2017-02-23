68038152e4cf82918434b2df77a16283afcd853a
Mike Pretzlaw <m.pretzlaw@inpsyde.com>
2017-02-23 23:53:16 +0100

    translation box: Process translations via page

--- admin/meta-box/translation-box.php
+++ admin/meta-box/translation-box.php
@@ -1,8 +1,7 @@
 <p class="textright">
-	<a href="<?php echo admin_url( 'plugins.php?page=tm4mlp_add_translation&type=post&id=' . get_the_ID() ) ?>"
+	<a href="<?php echo admin_url( 'index.php?page=tm4mlp_add_translation&type=post&id=' . get_the_ID() ) ?>"
 	   title="<?php esc_attr_e( 'Request translation from Eurotext', 'tm4mlp' ) ?>"
 	   class="button button-primary">
 		<?php esc_html_e( 'Request translation(s)', 'tm4mlp' ) ?>
 	</a>
 </p>
-
--- includes/admin/add-translation.php
+++ includes/admin/add-translation.php
@@ -14,17 +14,14 @@ function tm4mlp_add_translation_action() {
  * Manage menu items and pages.
  */
 function tm4mlp_add_translation_menu() {
-	global $_registered_pages;
-
-	$menu_slug = 'tm4mlp_add_translation';
-	$hook      = get_plugin_page_hookname( $menu_slug, '' );
-
-	if ( ! $hook ) {
-		throw new \Exception( 'TM4MLP: WordPress broke the admin logic.' );
-	}
-
-	add_action( $hook, 'tm4mlp_add_translation_action' );
-	$_registered_pages[ $hook ] = true;
+	add_submenu_page(
+		null,
+		__('Translations', 'tm4mlp'),
+		__('Translations', 'tm4mlp'),
+		'read',
+		'tm4mlp_add_translation',
+		'tm4mlp_add_translation_action'
+	);
 }
 
 add_action( 'admin_menu', 'tm4mlp_add_translation_menu' );
