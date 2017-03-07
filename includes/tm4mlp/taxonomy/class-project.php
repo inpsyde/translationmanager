4fa1eb0e39703921e33376905c6075447894d5d9
Mike Pretzlaw <m.pretzlaw@inpsyde.com>
2017-03-07 11:39:54 +0100

    Project tax: Goto project button

--- includes/tm4mlp/taxonomy/class-project.php
+++ includes/tm4mlp/taxonomy/class-project.php
@@ -5,7 +5,8 @@ namespace Tm4mlp\Taxonomy;
 class Project {
 	const TAXONOMY = TM4MLP_TAX_PROJECT;
 
-	const COL_ORDER_STATUS = 'tm4mlp_order_status';
+	const COL_STATUS = 'tm4mlp_order_status';
+	const COL_ACTIONS = 'tm4mlp_order_action';
 
 	public static function register_post_status() {
 	}
@@ -14,13 +15,14 @@ class Project {
 
 		unset( $columns['cb'] );
 		unset( $columns['slug'] );
-
-		$columns['posts'] = '';
+		unset( $columns['posts'] );
 
 		// Add status ad second place.
 		$columns = array_slice( $columns, 0, 1 )
-		           + array( static::COL_ORDER_STATUS => __( 'Status', 'tm4mlp' ) )
+		           + array( static::COL_STATUS => __( 'Status', 'tm4mlp' ) )
 		           + array_slice( $columns, 1 );
+
+		$columns[ static::COL_ACTIONS ] = '';
 
 		add_action(
 			'manage_' . static::TAXONOMY . '_custom_column',
@@ -34,7 +36,7 @@ class Project {
 
 	public static function print_column( $value, $column_name, $term_id ) {
 		switch ( $column_name ) {
-			case static::COL_ORDER_STATUS:
+			case static::COL_STATUS:
 				if ( ! get_term_meta( $term_id, '_tm4mlp_order_id', true ) ) {
 					return __( 'New', 'tm4mlp' );
 				}
@@ -44,7 +46,21 @@ class Project {
 					date( 'Y-m-d' )
 				);
 				break;
-
+			case static::COL_ACTIONS:
+				return sprintf(
+					'<a href="%s" class="button">%s</a>',
+					get_admin_url(
+						null,
+						'edit.php?' .
+						http_build_query(
+							array(
+								TM4MLP_TAX_PROJECT => get_term_field( 'slug', $term_id ),
+								'post_type'        => TM4MLP_CART,
+							)
+						)
+					),
+					__( 'Show project', 'tm4mlp' )
+				);
 		}
 
 		return $value;
