89382b4e409f27cabd0c877adb440fb8aa5f3b32
Khan Mohammad Rashedun-Naby <naby88@gmail.com>
2017-06-09 00:57:50 +0600

    Remove menu entries from taxonomy view

--- includes/functions.php
+++ includes/functions.php
@@ -10,4 +10,19 @@ function inpsyde_remove_search_box() {
 	) {
 		echo '<style type="text/css">.post-type-tm4mlp_cart #posts-filter .search-box {display: none !important;}</style>';
 	}
+
+	if( 'edit-tags' == $screen->base
+	    && isset($_GET['taxonomy'])
+	    && 'tm4mlp_project' == $_GET['taxonomy']
+	    && isset($_GET['post_type'])
+	    && 'tm4mlp_cart' == $_GET['post_type']
+	) {
+		echo '
+			<style type="text/css">
+				.post-type-tm4mlp_cart .row-actions span.edit, 
+				.post-type-tm4mlp_cart .row-actions span.inline.hide-if-no-js, 
+				.post-type-tm4mlp_cart .row-actions span.view {display: none !important;}
+			</style>
+			';
+	}
 }
