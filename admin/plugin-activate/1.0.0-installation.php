4e223df45de1e564f9a5949ed238b25d409856df
Robert Windisch <r.windisch@inpsyde.com>
2017-12-05 12:23:49 +0100

    TM-89 Use current URL for REST-API

--- admin/plugin-activate/1.0.0-installation.php
+++ admin/plugin-activate/1.0.0-installation.php
@@ -1,5 +1,5 @@
 <?php
 
 if ( ! get_site_option( 'translationmanager_api_url' ) ) {
-	update_site_option( 'translationmanager_api_url', 'http://46.4.76.165/api' );
+	update_site_option( 'translationmanager_api_url', 'http://api.eurotext.de/api' );
 }
