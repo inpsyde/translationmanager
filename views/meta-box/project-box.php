d404946e8b2ab0f80c035ccb73dd432248fef432
Guido Scialfa <dev@guidoscialfa.com>
2018-03-02 13:30:15 +0100

    Remove unnecessary div container in project box.

--- views/meta-box/project-box.php
+++ views/meta-box/project-box.php
@@ -1,6 +1,5 @@
 <div id="postbox-container-1"
      class="postbox-container translationmanager-postbox-container">
-	<div id="" class="">
 	<div id="submitdiv" class="postbox">
 		<h2 class="box-status-title">
 			<?php esc_html_e( 'Status', 'translationmanager' ); ?>
@@ -8,6 +7,5 @@
 		<div class="inside">
 			<?php $info->dispatch(); ?>
 		</div>
-	</div>
 	</div>
 </div>
