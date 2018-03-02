5bd00ec2eb420de3cdcdad0aba652e3c21fd4cd5
Guido Scialfa <dev@guidoscialfa.com>
2018-03-02 12:58:10 +0100

    Reintroduce api. Capitalizing the directory name for case sensitive OS's

---
+++ src/Api/Project.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Handling the project endpoint of the API.
+ *
+ * @since   1.0.0
+ *
+ * @package Translationmanager\Api
+ */
+
+namespace Translationmanager\Api;
+
+use Translationmanager\Api;
+
+/**
+ * Class Project
+ *
+ * @since   1.0.0
+ *
+ * @package Translationmanager\Api
+ */
+class Project {
+
+	/**
+	 * Endpoint Url
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The endpoint for the project
+	 */
+	const URL = 'project';
+
+	/**
+	 * Api
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var Api The instance of the api
+	 */
+	private $api;
+
+	/**
+	 * Project constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param \Translationmanager\Api $api he instance of the api.
+	 */
+	public function __construct( Api $api ) {
+
+		$this->api = $api;
+	}
+
+	/**
+	 * Create a new project.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param \Translationmanager\Domain\Project $project The project info needed to create the project in the server.
+	 *
+	 * @return int|null ID of the new project or NULL on failure.
+	 */
+	public function create( \Translationmanager\Domain\Project $project ) {
+
+		$body = $this->api->post( self::URL, [], $project->to_header_array() );
+
+		if ( ! isset( $body['id'] ) ) {
+			return null;
+		}
+
+		return (int) $body['id'];
+	}
+
+	/**
+	 * Get Project
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $project_id The ID of the project to retrieve from the server.
+	 *
+	 * @return string[] The response data
+	 */
+	public function get( $project_id ) {
+
+		return $this->api->get( 'project/' . (int) $project_id );
+	}
+}
---
+++ src/Api/ProjectItem.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Translationmanager\Api;
+
+use Translationmanager\Functions;
+use Translationmanager\Api;
+
+/**
+ * Handling the project endpoint of the API.
+ *
+ * @package Translationmanager\Api
+ */
+class ProjectItem {
+
+	/**
+	 * URL
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The endpoint for the project item.
+	 */
+	const URL = 'project/%d/item';
+
+	/**
+	 * API
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var Api
+	 */
+	private $api;
+
+	/**
+	 * ProjectItem constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param \Translationmanager\Api $api
+	 */
+	public function __construct( Api $api ) {
+
+		$this->api = $api;
+	}
+
+	/**
+	 * Create a new project.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int    $project_id
+	 * @param string $post_type_name
+	 * @param string $target_language
+	 * @param array  $data
+	 *
+	 * @return int|null ID of the new project or NULL on failure.
+	 */
+	public function create( $project_id, $post_type_name, $target_language, $data = [] ) {
+
+		$body = $this->get_api()->post(
+			$this->get_url( $project_id ),
+			$data,
+			[
+				'X-Source'        => $this->normalize_language_code( Functions\current_lang_code() ),
+				'X-Target'        => $this->normalize_language_code( $target_language ),
+				'X-TextType'      => $this->get_text_type( $post_type_name ),
+				'X-System-Module' => $post_type_name,
+			]
+		);
+
+		if ( ! isset( $body['id'] ) ) {
+			return null;
+		}
+
+		return (int) $body['id'];
+	}
+
+	/**
+	 * @since 1.0.0
+	 *
+	 * @return Api
+	 */
+	private function get_api() {
+
+		return $this->api;
+	}
+
+	/**
+	 * @since 1.0.0
+	 *
+	 * @param string $project_id Project ID.
+	 *
+	 * @return string The url for the request
+	 */
+	private function get_url( $project_id ) {
+
+		return sprintf( self::URL, $project_id );
+	}
+
+	/**
+	 * Get the Text-Type based on the Post-Type
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $post_type_name The post type name.
+	 *
+	 * @return string text-type name for REST-API
+	 */
+	private function get_text_type( $post_type_name ) {
+
+		$text_type_name = str_replace(
+			[ 'post', 'page' ],
+			[ 'marketing', 'specialized-text' ],
+			$post_type_name
+		);
+
+		$text_type_name = apply_filters( 'translationmanager_get_text_type', $text_type_name, $post_type_name );
+
+		return $text_type_name;
+	}
+
+	/**
+	 * Normalize language code for translation manager api request
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $lang_code The language code to normalize.
+	 *
+	 * @return string The normalize language code
+	 */
+	private function normalize_language_code( $lang_code ) {
+
+		return strtolower( str_replace( '-', '_', $lang_code ) );
+	}
+}
