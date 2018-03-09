a61e70d9abdfb358a4ba7863974359969242a30d
Guido Scialfa <dev@guidoscialfa.com>
2018-03-09 18:02:42 +0100

    TM-130: Remove the infografics and fix the site url

--- views/options-page/tabs/about.php
+++ views/options-page/tabs/about.php
@@ -1,6 +1,4 @@
 <div class="backwpup-welcome">
-	<img class="backwpup-banner-img"
-	     src="<?php echo esc_url( $this->plugin->url( '/resources/img/Workflow_Industrie_EN.jpg' ) ) ?>"/>
 	<h3><?php esc_html_e( 'Welcome to translationMANAGER for WordPress', 'translationmanager' ); ?></h3>
 
 	<p><?php esc_html_e( 'With the translationMANAGER, exporting content from your WordPress Multisite is simple and straightforward. Select posts, pages and customized content (custom post types) with just a few clicks and send the texts to the Eurotext Translation Portal. Once translated, texts can be reimported just as quickly and then immediately published. All of this from the comfort of the WordPress admin area. Professional translations with WordPress have never been so simple!', 'translationmanager' ); ?></p>
@@ -36,7 +34,7 @@
 			<?php esc_html_e( 'Email:', 'translationmanager' ); ?>
 			<a href="mailto:info@eurotext.de">info@eurotext.de</a><br/>
 			<?php esc_html_e( 'Web:', 'translationmanager' ); ?>
-			<a href="http://www.eurotext.de/en/">www.eurotext.de/en/</a>
+			<a href="https://www.eurotext-ecommerce.com">https://www.eurotext-ecommerce.com</a>
 		</td>
 	</tr>
 	</tbody>
