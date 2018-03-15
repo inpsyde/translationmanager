f73b8dd660c059d1e0386b115aed988bf6c6d8dc
Guido Scialfa <dev@guidoscialfa.com>
2018-03-15 09:40:15 +0100

    Remove always commented code and relative views.

--- views/project/extra-tablenav.php
+++
@@ -1,6 +0,0 @@
-<?php /** @var array $request */ ?>
-<div class="alignleft actions">
-	<?php if ( ! $request['translationmanager_project'] ): ?>
-		<?php esc_html_e( 'Please select the project you like to order.', 'translationmanager' ) ?>
-	<?php endif; ?>
-</div>
