9c93b339d89caf83accbfa150de6dc75c1edf4df
Marc Eichenseher <eichenseher.marc@gmail.com>
2020-05-19 09:19:02 +0200

    Support gutenberg add posts to projects
    Difference classic editor to gutenberg: Gutenberg save posts
    asynchronously. That leads to the problem that the old way
    (clicking on a button which holds a name) did not work.
    Quick fix: When adding a post to a project make that
    (by adding an input field with that name) available again.

--- views/meta-box/translation/layout.php
+++ views/meta-box/translation/layout.php
@@ -1,61 +1,76 @@
 <?php
 if ( ! $this->get_customer_key() ):
 
-	require_once \Translationmanager\Functions\get_template( '/views/meta-box/translation/no-key-customer.php' );
+    require_once \Translationmanager\Functions\get_template( '/views/meta-box/translation/no-key-customer.php' );
 
 else :
-	?>
-	<!-- TODO values need to be transferred. -->
-	<div class="translationmanager-translation-box">
-		<?php
-		if ( ! empty( \Translationmanager\Functions\get_languages() ) ) :
-			if ( \Translationmanager\Functions\projects() ) {
-				require_once \Translationmanager\Functions\get_template( 'views/meta-box/translation/projects.php' );
-			}
+    ?>
+    <!-- TODO values need to be transferred. -->
+    <div class="translationmanager-translation-box">
+        <?php
+        if ( ! empty( \Translationmanager\Functions\get_languages() ) ) :
+            if ( \Translationmanager\Functions\projects() ) {
+                require_once \Translationmanager\Functions\get_template( 'views/meta-box/translation/projects.php' );
+            }
 
-			foreach ( \Translationmanager\Functions\get_languages() as $key => $language ) : ?>
-				<div>
-					<label for="language_<?php esc_attr_e( $language->get_lang_code() ); ?>">
-						<input type="checkbox"
-						       class="translationmanager_languages_class"
-						       name="translationmanager_language[]"
-						       value="<?php esc_attr_e( $key ) ?>"
-						       id="language_<?php esc_attr_e( $language->get_lang_code() ); ?>"/>
-						<?php esc_html_e( $language->get_label() ); ?>
-					</label>
-				</div>
-			<?php endforeach; ?>
+            foreach ( \Translationmanager\Functions\get_languages() as $key => $language ) : ?>
+                <div>
+                    <label for="language_<?php esc_attr_e( $language->get_lang_code() ); ?>">
+                        <input type="checkbox"
+                               class="translationmanager_languages_class"
+                               name="translationmanager_language[]"
+                               value="<?php esc_attr_e( $key ) ?>"
+                               id="language_<?php esc_attr_e( $language->get_lang_code() ); ?>"/>
+                        <?php esc_html_e( $language->get_label() ); ?>
+                    </label>
+                </div>
+            <?php endforeach; ?>
 
-			<p id="translationmanager-inquery-button-id">
-				<button type="submit"
-				        name="translationmanager_action_project_add_translation"
-				        title="<?php esc_attr_e( 'Create a new project containing the selected languages.', 'translationmanager' ) ?>"
-				        class="button button-primary translationmanager-inquery-button">
-					<?php echo esc_html( $this->context_button_label() ); ?>
-				</button>
-			</p>
+            <p id="translationmanager-inquery-button-id">
+                <button type="submit"
+                        name="translationmanager_action_project_add_translation"
+                        title="<?php esc_attr_e( 'Create a new project containing the selected languages.', 'translationmanager' ) ?>"
+                        class="button button-primary translationmanager-inquery-button">
+                    <?php echo esc_html( $this->context_button_label() ); ?>
+                </button>
+            </p>
 
-			<input type="hidden"
-			       name="<?php echo esc_attr( $this->nonce()->action() ); ?>"
-			       value="<?php echo esc_attr( $this->nonce() ); ?>"/>
+            <input type="hidden"
+                   name="<?php echo esc_attr( $this->nonce()->action() ); ?>"
+                   value="<?php echo esc_attr( $this->nonce() ); ?>"/>
 
-		<?php else : ?>
-			<p><?php esc_html_e( 'No language found !', 'translationmanager' ); ?></p>
-		<?php endif; ?>
-	</div>
+        <?php else : ?>
+            <p><?php esc_html_e( 'No language found !', 'translationmanager' ); ?></p>
+        <?php endif; ?>
+    </div>
 
-	<script>
-		(function ( $ ) {
-			$( function () {
-				$( '#translationmanager-inquery-button-id' ).on( 'click', '.translationmanager-inquery-button', function () {
-					var checked = $( "input.translationmanager_languages_class" ).is( ':checked' );
-					if ( ! checked ) {
-						alert( "You must check at least one language." );
-						return false;
-					}
-				} );
-			} )
-		})( jQuery );
-	</script>
+    <script>
+        (function ( $ ) {
+            $( function () {
+                $( '#translationmanager-inquery-button-id' )
+                    .on( 'click', '.translationmanager-inquery-button', function () {
+                        const checked = $( "input.translationmanager_languages_class" ).is( ':checked' );
+                        if ( ! checked ) {
+                            alert( "You must check at least one language." );
+                            return false;
+                        }
+
+                        const isGutenbergReady = typeof wp !== 'undefined' && typeof wp.blocks !== 'undefined';
+                        if (! isGutenbergReady) {
+                          return;
+                        }
+
+                        const actionAddProjectSelector = 'translationmanager_action_project_add_translation';
+
+                        $('.translationmanager-translation-box')
+                            .append(`<input id="${actionAddProjectSelector}" name="${actionAddProjectSelector}" value="true" type="hidden" />`);
+
+                        wp.data.dispatch('core/editor').savePost().then(function() {
+                            $(`#${actionAddProjectSelector}`).remove();
+                        });
+                } );
+            } )
+        })( jQuery );
+    </script>
 
 <?php endif; ?>
