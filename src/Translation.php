2a15d39f8d91278e968baf026d2a4ea8b6474cc0
Narek Zakarian <narekzakarian@gmail.com>
2021-01-27 17:42:24 +0400

    Fix PHPCS problems

--- src/Translation.php
+++ src/Translation.php
@@ -386,8 +386,9 @@ class Translation implements ArrayAccess, JsonSerializable
         if (!$namespace && in_array($key, self::$protected_meta, true)) {
             _doing_it_wrong(
                 __METHOD__,
-                // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
+                // @codingStandardsIgnoreStart
                 "Meta key {$key} is protected and can't be overridden.",
+                // @codingStandardsIgnoreEnd
                 '0.1'
             );
 
