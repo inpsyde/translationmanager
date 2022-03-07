e90f96ded86df48e12c72451c38007b82750594c
Alex P <alex.pantec@gmail.com>
2022-03-07 10:12:40 +0200

    Remove old unused files

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM pretzlaw/wordpress:7.0-apache
-
-WORKDIR /var/www
-
-RUN docker-php-ext-enable mysqli
-RUN docker-php-ext-enable xdebug
-RUN a2enmod rewrite
--- bin/hook-git.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd `git rev-parse --show-toplevel`/.git/hooks
-
-ln -s ../../bin/test-min.sh pre-commit
-ln -s ../../bin/test-med.sh pre-push
